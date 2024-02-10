7ec11eac45758d06e1cf33c11a19d3390e9d1ea8
JBMagination <jbmagination@jbmagination.com>
2024-02-10 03:32:59 -0500

    Update release_check.sh

--- scripts/release_check.sh
+++ scripts/release_check.sh
@@ -2,5 +2,5 @@
 curl -Ls https://api.github.com/repos/Myzel394/locus/releases/latest | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release2.txt
 if [ -z "$(diff release.txt release2.txt)" ]; then
   echo No new release, skip.
-  exit 1
+  #exit 1
 fi
