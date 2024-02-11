7628374bed17fc30b8df40cb87bda7e3208c728a
JBMagination <jbmagination@jbmagination.com>
2024-02-10 20:56:50 -0500

    Update release_check.sh

--- scripts/release_check.sh
+++ scripts/release_check.sh
@@ -3,7 +3,7 @@ echo $PWD
 curl -Ls https://api.github.com/repos/Myzel394/locus/releases/latest | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release2.txt
 if [ -z "$(diff release.txt release2.txt)" ]; then
   echo No new release, stop.
-  exit 1
+  #exit 1
 else
   echo New release, continue.
 fi
