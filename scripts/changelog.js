2080141eead1a8fc9a633b8cdb3dd1ccea853ce8
JBMagination <jbmagination@jbmagination.com>
2024-02-12 05:56:38 -0500

    Update changelog.js

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -3,6 +3,6 @@ const path = require('path');
 fetch(`https://api.github.com/repos/Myzel394/locus/releases/${fs.readFileSync(path.resolve(path.resolve(__dirname), '../release.txt'))}`)
 .then(res => res.json())
 .then(data => {
-    let changelog = "> [!IMPORTANT]\n> This repository is not verified by or affiliated with Myzel394 or any other Locus developers. The below changelog is from [the original repository](https://github.com/Myzel394/locus).\n\n<hr>\n\n" + data['body'];
+    let changelog = `> [!IMPORTANT]\n> This repository is not verified by or affiliated with Myzel394 or any other Locus developers. The below changelog is from [the original repository](${data['html_url']}).\n\n<hr>\n\n${data['body']}`;
     fs.writeFileSync(path.resolve(path.resolve(__dirname), '../tmp/changelog.txt'), changelog);
 })
