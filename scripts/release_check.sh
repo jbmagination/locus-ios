d312b0d5a49710f7e988f5b3681ddebdd73a8ad0
Anka <runner@Mac-1708430860983.local>
2024-02-20 12:35:44 +0000

    Fix release ID

--- scripts/release_check.sh
+++ scripts/release_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo $PWD
-curl -Ls https://api.github.com/repos/Myzel394/locus/releases/latest | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release2.txt
+curl -Ls https://api.github.com/repos/Myzel394/locus/releases/latest > tmp/release.json 
+cat tmp/release.json | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release2.txt
 if [ -z "$(diff release.txt release2.txt)" ]; then
   echo No new release, stop.
   exit 1
