ecc8f5b9fcf622c14aff5947bd56cd43c14b61d5
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-07-03 00:29:43 -0700

    Update release_check.sh

--- scripts/release_check.sh
+++ scripts/release_check.sh
@@ -2,6 +2,11 @@
 echo $PWD
 curl -Ls https://api.github.com/repos/Myzel394/locus/releases/latest > tmp/release.json 
 cat tmp/release.json | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release2.txt
+echo "---"
+cat release.txt
+cat release2.txt
+cat tmp/release.json
+echo "---"
 if [ -z "$(diff release.txt release2.txt)" ]; then
   echo No new release, stop.
   exit 1
