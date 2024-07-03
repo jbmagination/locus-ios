46275fa6df0164449b34ece71abf85f3567f4ea9
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-07-03 00:41:33 -0700

    Update release_check.sh

--- scripts/release_check.sh
+++ scripts/release_check.sh
@@ -10,8 +10,22 @@ cat release2.txt
 cat tmp/release.json
 echo "---"
 if [ -z "$(diff release.txt release2.txt)" ]; then
-  echo No new release, stop.
+  echo "No new release, stop!"
   exit 1
 else
-  echo New release, continue.
+  echo "New release found! Verifying release exists."
+  export RELEASE=`cat release2.txt`
+  if [ "x$ENV_VARIABLE" != "x" ]; then
+    echo "Release variable empty; redo next cron!"
+    exit 1
+  else
+    curl -Ls https://api.github.com/repos/Myzel394/locus/releases/$RELEASE > tmp/release2.json
+    cat tmp/release.json | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release3.txt
+    if [ -z "$(diff release.txt release2.txt)" ]; then
+      echo "New release, continue!"
+    else
+      echo "Something went very wrong; redo next cron!"
+      exit 1
+    fi
+  fi
 fi
