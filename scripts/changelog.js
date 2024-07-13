1d7e374fc4e65a215a5c033049f3b16bfbd38b74
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-07-13 04:00:40 -0400

    Update changelog.js

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-fetch(`https://api.github.com/repos/Myzel394/locus/releases/${fs.readFileSync(path.resolve(path.resolve(__dirname), '../release.txt'))}`)
+fetch(`https://api.github.com/repos/Myzel394/locus/releases/${fs.readFileSync(path.resolve(path.resolve(__dirname), '../release.txt'), 'utf-8')}`, {
+    headers: {
+        "Authorization": process.env.GH_TOKEN
+    }
+})
 .then(res => res.json())
 .then(data => {
     let changelog = `> [!IMPORTANT]\n> This repository is not verified by or affiliated with Myzel394 or any other Locus developers. The below changelog is from [the original repository](${data['html_url']}).\n\n<hr>\n\n${data['body']}`;
