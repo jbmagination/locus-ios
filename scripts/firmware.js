a2b6f62830772055dfd1085c4fae88c6f587487f
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-07-13 06:14:51 -0400

    Create firmware.js

---
+++ scripts/firmware.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const bplist = require('bplist-parser');
+
+(async () => {
+    const obj = await bplist.parseFile(path.resolve(path.resolve(__dirname), '../tmp/Payload/Runner.app/Info.plist'));
+    const plist = obj[0];
+    let version = plist['MinimumOSVersion'];
+    fs.writeFileSync(path.resolve(path.resolve(__dirname), '../tmp/firmware.txt'), version);
+})();
