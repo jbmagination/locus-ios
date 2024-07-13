b7330c6c67a2495751c61b39e50085d6654ae680
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-07-13 04:00:59 -0400

    Update release_check.sh

--- scripts/release_check.sh
+++ scripts/release_check.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 mkdir $PWD/tmp
-curl -Ls https://api.github.com/repos/Myzel394/locus/releases/latest > tmp/release.json 
+curl -H "Authorization: Bearer $GH_TOKEN" -Ls https://api.github.com/repos/Myzel394/locus/releases/latest > tmp/release.json 
 cat tmp/release.json | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release2.txt
 echo "---"
-echo $PWD
-ls $PWD
 cat release.txt
 cat release2.txt
 cat tmp/release.json
@@ -19,7 +17,7 @@ else
     echo "Release variable empty; redo next cron!"
     exit 1
   else
-    curl -Ls https://api.github.com/repos/Myzel394/locus/releases/$RELEASE > tmp/release2.json
+    curl -H "Authorization: Bearer $GH_TOKEN" -Ls https://api.github.com/repos/Myzel394/locus/releases/$RELEASE > tmp/release2.json
     cat tmp/release.json | grep \"id\" -m 1 | head -1 | sed 's/[^0-9]*//g' > release3.txt
     diff release2.txt release3.txt
     if [ -z "$(diff release2.txt release3.txt)" ]; then
