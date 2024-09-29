d77f026389ed0bfc3cef9d64a98f0c6b122f0cd1
JBMagination <26027089+jbmagination@users.noreply.github.com>
2024-09-29 16:30:16 -0400

    Update release_commit.sh

--- scripts/release_commit.sh
+++ scripts/release_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -z "x$RERELEASE_CHECK" != "xfalse" ]; then
+if [ -z "x$RERELEASE_CHECK" = "xfalse" ]; then
   echo "Release check skipped; don't commit anything!"
 else
   git commit -m "[auto] Update release ID" -- release.txt
